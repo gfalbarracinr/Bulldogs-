a0075988b65e1f1172499ff8cba611323228d93e
gfalbarracinr <gfalbarracinr@unal.edu.co>
2017-12-08 19:02:34 -0500

    master

--- myJs.js
+++ myJs.js
@@ -1,17 +1,47 @@
-
 var showMobileNavClass = "show-mobile-nav";
 $(document).ready(function(){
 
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
+var btn5 = document.getElementById("myBtn5");
+var btn6 = document.getElementById("myBtn6");
+var btn7 = document.getElementById("myBtn7");
 var btn = document.getElementById("myBtn");
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
+
+
 btn.onclick = function() {
+    modal.style.display = "block";
+}
+btn1.onclick = function() {
+    modal.style.display = "block";
+}
+btn2.onclick = function() {
+    modal.style.display = "block";
+}
+btn3.onclick = function() {
+    modal.style.display = "block";
+}
+btn4.onclick = function() {
+    modal.style.display = "block";
+}
+btn5.onclick = function() {
+    modal.style.display = "block";
+}
+btn6.onclick = function() {
+    modal.style.display = "block";
+}
+btn7.onclick = function() {
     modal.style.display = "block";
 }
 
@@ -26,6 +56,7 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
 $(".go-upper-js").on("click",function(){
 	$("html, body").animate({ scrollTop: 0 }, "slow");
 });
@@ -36,5 +67,3 @@ $(".mobile-nav-bar").on("click",function(){
 });
 
 });
-
-
